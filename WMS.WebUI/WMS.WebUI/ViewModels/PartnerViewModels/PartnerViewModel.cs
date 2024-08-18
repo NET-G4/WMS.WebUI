a70a2b0e50b900a110c6790ea6798bbaccaf8d0a
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-08-18 15:39:48 +0500

    editing pages view

--- WMS.WebUI/WMS.WebUI/ViewModels/PartnerViewModels/PartnerViewModel.cs
+++ WMS.WebUI/WMS.WebUI/ViewModels/PartnerViewModels/PartnerViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace WMS.WebUI.ViewModels.PartnerViewModels;
+﻿using System.ComponentModel;
+
+namespace WMS.WebUI.ViewModels.PartnerViewModels;
 
 public class PartnerViewModel
 {
     public int Id { get; set; }
+    
+    [DisplayName("Full name")]
     public string FullName { get; set; }
+
+    [DisplayName("Phone number")]
     public string? PhoneNumber { get; set; }
     public decimal Balance { get; set; }
     public PartnerType Type { get; set; }
